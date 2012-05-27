3d6bcf14a6618193a2e7b2a712ecb4b061b5da4a
Dani <th0rnout@gmail.com>
2012-05-27 16:02:19 +0200

    Kilka drobnych zmian

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
 #define SFML_DYNAMIC
-#include "App.h"
-#include "windows.h"
 #define WIN32_LEAN_AND_MEAN
+#define PF_DEBUG
+#include "App.h"
+#include "Windows.h"
 
 int main(int argc, char *argv[])
 {
 	std::string arg1;
 	if(argv[1] != NULL) arg1 = argv[1];
 
+#ifndef PF_DEBUG
 	if(arg1 == "fed")
+#endif
 	{
 		App app(800, 600, false);
 		app.Run();
 	}
+#ifndef PF_DEBUG
 	else
 	{
-		MessageBox(0, "Run launcher first", "Error", MB_OK);
+		MessageBox(NULL, "Run launcher first!", "Error", MB_OK);
 		return 1;
 	}
+#endif
     return 0;
 }
